0a09473d47bf0a51efe60d63f9f683b100fed4f6
junkim93 <93kimhyunjun@gmail.com>
2020-05-02 23:11:07 +0900

    [UPDATE]--마크업을 위한 임시 mock 데이터 생성

---
+++ src/mockStack.js
@@ -0,0 +1,136 @@
+export default (function() {
+	const stack = [
+		{
+			_id: {
+				$oid: "5e8df68a9e10880d7c7e85f6",
+			},
+			name: "Javascript",
+			description: "basic language",
+			image: "image",
+			job_type: "developer",
+			job_detail: "frontend",
+			category: "language",
+			companies: [
+				{
+					_id: {
+						$oid: "5e8df7369e10880d7c7e8601",
+					},
+					name: "Google",
+					logo: "logo image url",
+					cnt: 9,
+				},
+				{
+					_id: {
+						$oid: "5e8df7369e10880d7c7e8602",
+					},
+					name: "Coupang",
+					logo: "logo image url",
+					cnt: 6,
+				}
+			],
+		},
+		{
+			_id: {
+				$oid: "5e8df68a9e10880d7c7e85f7",
+			},
+			name: "Typescript",
+			description: "basic language",
+			image: "image",
+			job_type: "developer",
+			job_detail: "backend",
+			category: "language",
+			companies: [
+				{
+					_id: {
+						$oid: "5e8df7369e10880d7c7e8601",
+					},
+					name: "Google",
+					logo: "logo image url",
+					cnt: 9,
+				}
+			],
+		},
+		{
+			_id: {
+				$oid: "5e8df68a9e10880d7c7e85f8",
+			},
+			name: "Python",
+			description: "basic language",
+			image: "image",
+			job_type: "developer",
+			job_detail: "backend",
+			category: "language",
+			companies: [
+				{
+					_id: {
+						$oid: "5e8df7369e10880d7c7e8602",
+					},
+					name: "Coupang",
+					logo: "logo image url",
+					cnt: 6,
+				}
+			],
+		},
+		{
+			_id: {
+				$oid: "5e8df68a9e10880d7c7e85f9",
+			},
+			name: "Nodejs",
+			description: "javascript backend framework",
+			image: "image",
+			job_type: "developer",
+			job_detail: "backend",
+			category: "framework",
+			companies: [],
+		},
+		{
+			_id: {
+				$oid: "5e8df68a9e10880d7c7e85fa",
+			},
+			name: "Django",
+			description: "python backend framework",
+			image: "image",
+			job_type: "developer",
+			job_detail: "backend",
+			category: "framework",
+			companies: [],
+		},
+		{
+			_id: {
+				$oid: "5e8df68a9e10880d7c7e85fb",
+			},
+			name: "React",
+			description: "javascript frontend framework",
+			image: "image",
+			job_type: "developer",
+			job_detail: "frontend",
+			category: "frontend",
+			companies: [],
+		},
+		{
+			_id: {
+				$oid: "5e8df68a9e10880d7c7e85fc",
+			},
+			name: "Vue",
+			description: "javascript frontend framework",
+			image: "image",
+			job_type: "developer",
+			job_detail: "frontend",
+			category: "framework",
+			companies: [],
+		},
+		{
+			_id: {
+				$oid: "5e8df68a9e10880d7c7e85fd",
+			},
+			name: "Angular",
+			description: "javascript frontend framework",
+			image: "image",
+			job_type: "developer",
+			job_detail: "frontend",
+			category: "framework",
+			companies: [],
+		}
+	];
+	return stack;
+})();

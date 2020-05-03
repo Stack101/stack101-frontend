6dbcbccfae9ffe2c0d7cf904caf0ea4b759f3b8d
junkim93 <93kimhyunjun@gmail.com>
2020-05-03 09:58:28 +0900

    [ADD]--서치 업데이트를 위한 일반 텍스트 컴포넌트 추가

---
+++ src/components/elements/AppText.vue
@@ -0,0 +1,34 @@
+<template>
+	<span :class="`e--description--${descClass}`">
+		{{ label }}
+	</span>
+</template>
+
+<script>
+	export default {
+		props: {
+			descClass: {
+				type: String,
+				default: null,
+			},
+			label: {
+				type: String,
+				default: null,
+			},
+		},
+	};
+</script>
+
+<style lang="scss" scoped>
+.e-description {
+	font-size: 16px;
+	font-weight: 500;
+	line-height: 1.45;
+	color: #3B3B3B;
+
+	&--search-result {
+		font-size: 14px;
+		color: #414143;
+	}
+}
+</style>

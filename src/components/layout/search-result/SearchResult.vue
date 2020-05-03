ec624dfe3cefda675e055dab0f95b0b0716b5576
junkim93 <93kimhyunjun@gmail.com>
2020-05-03 09:59:22 +0900

    [ADD]--상황별 서치 페이지 추가

---
+++ src/components/layout/search-result/SearchResult.vue
@@ -0,0 +1,56 @@
+<template>
+	<section>
+		<StackResult
+			v-if="isStackSearchList"
+			:stackList="stackSearchList"
+			:is-search-result="isSearchResult"
+		/>
+		<CompanyList
+			v-if="isCompanySearchList"
+			:list="companySearchList"
+			:is-search-result="isSearchResult"
+		/>
+	</section>
+</template>
+
+<script>
+import StackResult from "@/components/blocks/stack-result/StackResult";
+import CompanyList from "@/components/layout/company-list/CompanyList";
+
+export default {
+	components: {
+		StackResult,
+		CompanyList,
+	},
+
+	props: {
+		stackSearchList: {
+			type: Array,
+			default: null,
+		},
+		companySearchList: {
+			type: Array,
+			default: null,
+		},
+	},
+
+	data() {
+		return {
+			isSearchResult: true,
+		};
+	},
+
+	computed: {
+		isStackSearchList() {
+			return this.stackSearchList.length > 0;
+		},
+		isCompanySearchList() {
+			return this.companySearchList.length > 0;
+		},
+	},
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>

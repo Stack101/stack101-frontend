08ea6893ea6fa021c1474769d484be809df90c20
junkim93 <93kimhyunjun@gmail.com>
2020-05-07 22:36:22 +0900

    [UPDATE]--api url 설정

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "ec2-52-78-70-201.ap-northeast-2.compute.amazonaws.com/ec2-52-78-70-201.ap-northeast-2.compute.amazonaws.com",
 });
